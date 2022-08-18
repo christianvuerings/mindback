f06970642e3b753d564ec1fbe2b733ea35c0a147
Christian Vuerings <cvuerings@pinterest.com>
2022-08-18 11:36:55 +0200

    Make options togge-able without a full reload

--- source/content.js
+++ source/content.js
@@ -1,136 +1,210 @@
 import optionsStorage from "./options-storage.js";
 
-console.log("💈 Content script loaded for", chrome.runtime.getManifest().name);
-
 const translations = {
   unreadItems: ["unread items", "ongelezen items"],
 };
 
-function initTwitter(options) {
-  if (options.twitterHideTimelineCounts) {
-    Array.from(document.querySelectorAll("span"))
-      .filter(
-        (el) =>
-          // English
-          el.textContent === "Following" ||
-          el.textContent === "Followers" ||
-          // Dutch
-          el.textContent === "Volgend" ||
-          el.textContent === "Volgers"
-      )
-      .map((el) => el.closest("a")?.style.setProperty("display", "none"));
-  }
-
-  if (options.twitterHideTimelineCounts) {
-    Array.from(
-      document.querySelectorAll('[data-testid="app-text-transition-container"]')
-    ).forEach((el) =>
-      el.parentElement.style.setProperty("visibility", "hidden")
+function twitterHideFollowingFollowersCounts(options) {
+  Array.from(document.querySelectorAll("span"))
+    .filter(
+      (el) =>
+        // English
+        el.textContent === "Following" ||
+        el.textContent === "Followers" ||
+        // Dutch
+        el.textContent === "Volgend" ||
+        el.textContent === "Volgers"
+    )
+    .map((el) =>
+      el
+        .closest("a")
+        ?.setAttribute(
+          "data-mindback-display-none",
+          options.twitterHideFollowingFollowersCounts
+        )
     );
-  }
-  if (options.twitterHideNotificationsBadge) {
-    Array.from(
-      document.querySelectorAll(
-        translations.unreadItems
-          .map((item) => `[aria-label*="${item}"]`)
-          .join(",")
-      )
-    )
-      .filter((el) =>
-        ["Notifications", "Meldingen"].some((item) =>
-          el.parentElement.parentElement.textContent.includes(item)
-        )
-      )
-      .forEach((el) => el.style.setProperty("display", "none"));
-  }
-  if (options.twitterHideHomeBadge) {
-    Array.from(
-      document.querySelectorAll(
-        translations.unreadItems
-          .map((item) => `[aria-label*="${item}"]`)
-          .join(",")
-      )
-    )
-      .filter((el) =>
-        ["Home", "Startpagina"].some(
-          (item) =>
-            el?.parentElement?.parentElement.textContent.includes(item) ||
-            el?.closest(`[aria-label*="${item}"]`)
-        )
-      )
-      .forEach((el) => el.style.setProperty("display", "none"));
-  }
-  if (options.twitterHideRelevantPeopleSection) {
-    document
-      .querySelector('[aria-label="Relevant people"]')
-      ?.parentElement.style.setProperty("display", "none");
-  }
-  if (options.twitterHideWhatsHappeningSection) {
-    document
-      .querySelector(
-        ["Timeline: Trending now", "Tijdlijn: Nu populair"]
-          .map((item) => `[aria-label="${item}"]`)
-          .join(",")
-      )
-      ?.parentElement.style.setProperty("display", "none");
-  }
-  if (options.twitterHideWhoToFollowSection) {
-    document
-      .querySelector(
-        ["Who to follow", "Wie te volgen"]
-          .map((item) => `[aria-label="${item}"]`)
-          .join(",")
-      )
-      ?.parentElement.style.setProperty("display", "none");
-  }
-  if (options.twitterHideSignUpSection) {
-    document
-      .querySelector('[aria-label="Sign up"]')
-      ?.parentElement.style.setProperty("display", "none");
-  }
-  if (options.twitterHideSeeMoreTweetsModal) {
-    const foundSeeMoreTweets = Array.from(
-      document.querySelectorAll('[role="dialog"]')
-    )
-      .filter((el) => el.textContent.includes("See more Tweets from"))
-      .map((el) => el.style.setProperty("display", "none"));
-    if (foundSeeMoreTweets.length > 0) {
-      document.querySelector("html")?.style.setProperty("overflow", null);
-    }
-  }
-  if (options.twitterHideSeeWhatsHappeningModal) {
-    const foundSeeWhatsHappening = Array.from(
-      document.querySelectorAll('[role="dialog"]')
-    )
-      .filter((el) => el.textContent.includes("See what’s happening"))
-      .map((el) => el.style.setProperty("display", "none"));
-    if (foundSeeWhatsHappening.length > 0) {
-      document.querySelector("html")?.style.setProperty("overflow", null);
-    }
-  }
+}
 
-  if (options.twitterHideCookiebar) {
-    Array.from(document.querySelectorAll('[data-testid="BottomBar"]'))
-      .filter((el) => el.textContent.toLowerCase().includes("cookie"))
-      .map((el) => el.style.setProperty("display", "none"));
-  }
-  if (options.twitterHidePromotedTweets) {
-    Array.from(document.querySelectorAll("span"))
-      .filter((el) => el.textContent === "Promoted")
-      .forEach((el) =>
-        el
-          .closest('[data-testid="cellInnerDiv"]')
-          .style.setProperty("display", "none")
-      );
+function twitterHideTimelineCounts(options) {
+  Array.from(
+    document.querySelectorAll('[data-testid="app-text-transition-container"]')
+  ).forEach((el) =>
+    el.parentElement.setAttribute(
+      "data-mindback-visibility-hidden",
+      options.twitterHideTimelineCounts
+    )
+  );
+}
+
+function twitterHideNotificationsBadge(options) {
+  Array.from(
+    document.querySelectorAll(
+      translations.unreadItems
+        .map((item) => `[aria-label*="${item}"]`)
+        .join(",")
+    )
+  )
+    .filter((el) =>
+      ["Notifications", "Meldingen"].some((item) =>
+        el.parentElement.parentElement.textContent.includes(item)
+      )
+    )
+    .forEach((el) =>
+      el.setAttribute(
+        "data-mindback-display-none",
+        options.twitterHideNotificationsBadge
+      )
+    );
+}
+
+function twitterHideHomeBadge(options) {
+  Array.from(
+    document.querySelectorAll(
+      translations.unreadItems
+        .map((item) => `[aria-label*="${item}"]`)
+        .join(",")
+    )
+  )
+    .filter((el) =>
+      ["Home", "Startpagina"].some(
+        (item) =>
+          el?.parentElement?.parentElement.textContent.includes(item) ||
+          el?.closest(`[aria-label*="${item}"]`)
+      )
+    )
+    .forEach((el) =>
+      el.setAttribute(
+        "data-mindback-display-none",
+        options.twitterHideHomeBadge
+      )
+    );
+}
+
+function twitterHideRelevantPeopleSection(options) {
+  document
+    .querySelector('[aria-label="Relevant people"]')
+    ?.parentElement.setAttribute(
+      "data-mindback-display-none",
+      options.twitterHideRelevantPeopleSection
+    );
+}
+
+function twitterHideWhatsHappeningSection(options) {
+  document
+    .querySelector(
+      ["Timeline: Trending now", "Tijdlijn: Nu populair"]
+        .map((item) => `[aria-label="${item}"]`)
+        .join(",")
+    )
+    ?.parentElement.setAttribute(
+      "data-mindback-display-none",
+      options.twitterHideWhatsHappeningSection
+    );
+}
+
+function twitterHideWhoToFollowSection(options) {
+  document
+    .querySelector(
+      ["Who to follow", "Wie te volgen"]
+        .map((item) => `[aria-label="${item}"]`)
+        .join(",")
+    )
+    ?.parentElement.setAttribute(
+      "data-mindback-display-none",
+      options.twitterHideWhoToFollowSection
+    );
+}
+
+function twitterHideSignUpSection(options) {
+  document
+    .querySelector('[aria-label="Sign up"]')
+    ?.parentElement.setAttribute(
+      "data-mindback-display-none",
+      options.twitterHideSignUpSection
+    );
+}
+
+function twitterHideSeeMoreTweetsModal(options) {
+  const foundSeeMoreTweets = Array.from(
+    document.querySelectorAll('[role="dialog"]')
+  )
+    .filter((el) => el.textContent.includes("See more Tweets from"))
+    .map((el) =>
+      el.setAttribute(
+        "data-mindback-display-none",
+        options.twitterHideSeeMoreTweetsModal
+      )
+    );
+  if (options.twitterHideSeeMoreTweetsModal && foundSeeMoreTweets.length > 0) {
+    document.querySelector("html")?.style.setProperty("overflow", null);
   }
 }
 
-async function init() {
+function twitterHideSeeWhatsHappeningModal(options) {
+  const foundSeeWhatsHappening = Array.from(
+    document.querySelectorAll('[role="dialog"]')
+  )
+    .filter((el) => el.textContent.includes("See what’s happening"))
+    .map((el) =>
+      el.setAttribute(
+        "data-mindback-display-none",
+        options.twitterHideSeeWhatsHappeningModal
+      )
+    );
+  if (
+    options.twitterHideSeeWhatsHappeningModal &&
+    foundSeeWhatsHappening.length > 0
+  ) {
+    document.querySelector("html")?.style.setProperty("overflow", null);
+  }
+}
+
+function twitterHideCookiebar(options) {
+  Array.from(document.querySelectorAll('[data-testid="BottomBar"]'))
+    .filter((el) => el.textContent.toLowerCase().includes("cookie"))
+    .map((el) =>
+      el.setAttribute(
+        "data-mindback-display-none",
+        options.twitterHideCookiebar
+      )
+    );
+}
+
+function twitterHidePromotedTweets(options) {
+  Array.from(document.querySelectorAll("span"))
+    .filter((el) => el.textContent === "Promoted")
+    .forEach((el) =>
+      el
+        .closest('[data-testid="cellInnerDiv"]')
+        .setAttribute(
+          "data-mindback-display-none",
+          options.twitterHidePromotedTweets
+        )
+    );
+}
+
+async function initTwitter() {
   const options = await optionsStorage.getAll();
 
-  const observer = new MutationObserver(() => {
-    initTwitter(options);
-  });
+  twitterHideFollowingFollowersCounts(options);
+  twitterHideTimelineCounts(options);
+  twitterHideNotificationsBadge(options);
+  twitterHideHomeBadge(options);
+  twitterHideRelevantPeopleSection(options);
+  twitterHideWhatsHappeningSection(options);
+  twitterHideWhoToFollowSection(options);
+  twitterHideSignUpSection(options);
+  twitterHideSeeMoreTweetsModal(options);
+  twitterHideSeeWhatsHappeningModal(options);
+  twitterHideCookiebar(options);
+  twitterHidePromotedTweets(options);
+}
+
+async function init() {
+  console.log("Content script loaded for", chrome.runtime.getManifest().name);
+
+  chrome.storage.onChanged.addListener(initTwitter);
+  const observer = new MutationObserver(initTwitter);
 
   observer.observe(document.body, {
     childList: true,
